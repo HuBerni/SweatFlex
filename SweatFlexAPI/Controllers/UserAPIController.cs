4125abfc835e68c549a282fe9d425adaff94f0b8
Bernhard Huber <Bernhard.HUBER@lbs4.salzburg.at>
2023-09-19 16:00:47 +0200

    Created API including UserAPIController

---
+++ SweatFlexAPI/Controllers/UserAPIController.cs
@@ -0,0 +1,237 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SweatFlexAPI.Models;
+using SweatFlexData.DTOs;
+using SweatFlexData.DTOs.Create;
+using SweatFlexData.DTOs.Update;
+using SweatFlexData.Interface;
+using System.Net;
+
+namespace SweatFlexAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserAPIController : ControllerBase
+    {
+        private readonly IDataHandler _dataHandler;
+        private ApiResponse _response;
+
+        public UserAPIController(IDataHandler dataHandler)
+        {
+            _dataHandler = dataHandler;
+            _response = new();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<ApiResponse>> GetUsers()
+        {
+            try
+            {
+                var userDtos = await _dataHandler.GetUsersAsync();
+
+                if (userDtos == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    _response.ErrorMessages.Add("No users found");
+                    return BadRequest(_response);
+                }
+
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.Result = userDtos;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages.Add($"Error getting users: {ex.Message}");
+                return StatusCode((int)_response.StatusCode, _response);
+            }
+
+            return Ok(_response);
+        }
+
+        [HttpGet]
+        [Route("id:string", Name ="GetUserById")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<ApiResponse>> GetUser(string id)
+        {
+            try
+            {
+                var userDto = await _dataHandler.GetUserByIdAsync(id);
+
+                if (userDto == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    _response.ErrorMessages.Add("No user with this Id found");
+                    return BadRequest(_response);
+                }
+
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.Result = userDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages.Add($"Error getting user: {ex.Message}");
+                return StatusCode((int)_response.StatusCode, _response);
+            }
+
+            return Ok(_response);
+        }
+
+        [HttpGet]
+        [Route("id:string", Name = "GetUsersByCoach")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<ApiResponse>> GetUserByCoach(string coachId)
+        {
+            try
+            {
+                var userDtos = await _dataHandler.GetUsersByCoachIdAsync(coachId);
+
+                if (userDtos == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    _response.ErrorMessages.Add("No users for the coach found");
+                    return BadRequest(_response);
+                }
+
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.Result = userDtos;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages.Add($"Error getting users: {ex.Message}");
+                return StatusCode((int)_response.StatusCode, _response);
+            }
+
+            return Ok(_response);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<ApiResponse>> CreateUser(UserCreateDTO createDTO)
+        {
+            try
+            {
+                var userDto = await _dataHandler.CreateUserAsync(createDTO);
+
+                if (userDto == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.ErrorMessages.Add("Error creating user");
+                    return BadRequest(_response);
+                }
+
+                _response.StatusCode = HttpStatusCode.Created;
+                _response.Result = userDto;
+                return CreatedAtRoute("GetUserById", new { id = userDto.Id }, _response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages.Add($"Error creating user: {ex.Message}");
+                return StatusCode((int)_response.StatusCode, _response);
+            }
+        }
+
+        [HttpPut]
+        [Route("id:string")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<ApiResponse>> UpdateUser(string id, UserUpdateDTO updateDTO)
+        {
+            try
+            {
+                var userDto = await _dataHandler.GetUserByIdAsync(id);
+
+                if (userDto == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    _response.ErrorMessages.Add("User for update operation not found");
+                    return BadRequest(_response);
+                }
+
+                userDto = await _dataHandler.UpdateUserAsync(id, updateDTO);
+
+                if (userDto == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.ErrorMessages.Add("Error updating user");
+                    return BadRequest(_response);
+                }
+
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.Result = userDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages.Add($"Error updating user: {ex.Message}");
+                return StatusCode((int)_response.StatusCode, _response);
+            }
+
+            return Ok(_response);
+        }
+
+        [HttpDelete]
+        [Route("id:string")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<ApiResponse>> DeleteUser(string id)
+        {
+            try
+            {
+                var userDto = await _dataHandler.GetUserByIdAsync(id);
+
+                if (userDto == null)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    _response.ErrorMessages.Add("User for delete operation not found");
+                    return BadRequest(_response);
+                }
+
+                var result = await _dataHandler.DeleteUserAsync(id);
+
+                if (!result)
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    _response.ErrorMessages.Add("Error deleting user");
+                    return BadRequest(_response);
+                }
+
+                _response.StatusCode = HttpStatusCode.OK;
+                _response.Result = result;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.StatusCode = HttpStatusCode.InternalServerError;
+                _response.ErrorMessages.Add($"Error deleting user: {ex.Message}");
+                return StatusCode((int)_response.StatusCode, _response);
+            }
+
+            return Ok(_response);
+        }
+    }
+}
---
+++ SweatFlexAPI/Models/ApiResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace SweatFlexAPI.Models
+{
+    public class ApiResponse
+    {
+        public ApiResponse() => ErrorMessages = new List<string>();
+
+        public HttpStatusCode StatusCode { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public List<string> ErrorMessages { get; set; }
+        public object? Result { get; set; }
+    }
+}
