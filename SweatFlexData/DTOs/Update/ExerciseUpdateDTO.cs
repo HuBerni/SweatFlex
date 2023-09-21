9675d0cbf9c949f4df90b168918ca818d9219d44
Bernhard Huber <Bernhard.HUBER@lbs4.salzburg.at>
2023-09-21 13:31:45 +0200

    Changed CoachId to string from int

--- SweatFlexData/DTOs/Update/ExerciseUpdateDTO.cs
+++ SweatFlexData/DTOs/Update/ExerciseUpdateDTO.cs
@@ -8,16 +8,15 @@ namespace SweatFlexData.DTOs.Update
 {
     public class ExerciseUpdateDTO
     {
-        public int Id { get; set; }
-        public int Creator { get; set; }
+        public string Creator { get; set; }
 
         public string Name { get; set; }
 
-        public string Type { get; set; }
+        public int Type { get; set; }
 
-        public string Musclegroup { get; set; }
+        public int Musclegroup { get; set; }
 
-        public string? Equipment { get; set; }
+        public int? Equipment { get; set; }
 
         public string Description { get; set; }
 
