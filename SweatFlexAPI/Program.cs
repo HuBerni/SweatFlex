816dbca4b536010e19e8e9e2d0efb0789d4eb865
Bernhard Huber <Bernhard.HUBER@lbs4.salzburg.at>
2023-09-21 13:34:15 +0200

    Added context and IDataHandler Dependency Injection

--- SweatFlexAPI/Program.cs
+++ SweatFlexAPI/Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using SweatFlexData.Interface;
+using SweatFlexEF;
+using SweatFlexEF.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<SweatFlexContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SweatFlexDB"));
+});
+
+builder.Services.AddScoped<IDataHandler, DataHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
