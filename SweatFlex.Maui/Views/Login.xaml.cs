fa5a40db847db65ac53d9d0632792984cee80240
Bernhard Huber <Bernhard.HUBER@lbs4.salzburg.at>
2023-09-28 13:35:25 +0200

    Added Login and Register Views

---
+++ SweatFlex.Maui/Views/Login.xaml.cs
@@ -0,0 +1,9 @@
+namespace SweatFlex.Maui.Views;
+
+public partial class Login : ContentPage
+{
+	public Login()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SweatFlex.Maui/Views/Register.xaml.cs
@@ -0,0 +1,9 @@
+namespace SweatFlex.Maui.Views;
+
+public partial class Register : ContentPage
+{
+	public Register()
+	{
+		InitializeComponent();
+	}
+}
