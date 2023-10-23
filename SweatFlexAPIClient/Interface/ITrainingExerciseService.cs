22be313b1cb5154ebb011e32cb3d28d043ad474b
Alexander Höll <Alexander.HOELL@lbs4.salzburg.at>
2023-10-23 08:46:48 +0200

    Interface changed to current stage

--- SweatFlexAPIClient/Interface/ITrainingExerciseService.cs
+++ SweatFlexAPIClient/Interface/ITrainingExerciseService.cs
@@ -9,7 +9,7 @@ namespace SweatFlexAPIClient.Interface
     {
         Task<ApiResponse<IList<TrainingExerciseDTO>>> GetTrainingExercisesAsync(string userId, int? workoutId = null);
         Task<ApiResponse<TrainingExerciseDTO>> GetTrainingExerciseByIdAsync(int id);
-        Task<ApiResponse<TrainingExerciseDTO>> CreateTrainingExerciseAsync(TrainingExerciseCreateDTO createDTO);
+        Task<ApiResponse<List<TrainingExerciseDTO>>> CreateTrainingExerciseAsync(List<TrainingExerciseCreateDTO> createDTO);
         Task<ApiResponse<TrainingExerciseDTO>> UpdateTrainingExerciseAsync(int id, TrainingExerciseUpdateDTO updateDTO);
         Task<ApiResponse<bool>> DeleteTrainingExerciseAsync(int id);
     }
