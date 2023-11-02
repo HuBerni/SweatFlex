3863bf82bc6bb8bd1dc8235a12efc681a2c3c35b
Alexander Hoell <Alexander.Hoell@liebherr.com>
2023-11-02 11:27:42 +0100

    SQLite added

---
+++ SweatFlex.Maui/Models/TrainingExerciseLocal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweatFlex.Maui.Models
+{
+    public class TrainingExerciseLocal
+    {
+        public int Id { get; set; }
+        public bool Done { get; set; }
+        public decimal? Weight { get; set; }
+        public int? Reputations { get; set; }
+        public int? TimeInSec { get; set; }
+        public User User { get; set; }
+        public DateTime? ExerciseExecuted { get; set; }
+        public Exercise Exercise { get; set; }
+        public int WorkoutExerciseId { get; set; }
+        public int SessionId { get; set; }
+    }
+}
---
+++ SweatFlex.Maui/SQLLite/TodoItemDatabase.cs
@@ -0,0 +1,59 @@
+﻿using SQLite;
+using SweatFlex.Maui.Models;
+
+namespace SweatFlex.Maui.SQLLite
+{
+    public class TodoItemDatabase
+    {
+        SQLiteAsyncConnection Database;
+
+        public TodoItemDatabase()
+        {
+        }
+
+        async Task Init()
+        {
+            if (Database is not null)
+                return;
+
+            Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+            var result = await Database.CreateTableAsync<TrainingExerciseLocal>();
+        }
+
+        public async Task<List<TrainingExerciseLocal>> GetItemsAsync()
+        {
+            await Init();
+            return await Database.Table<TrainingExerciseLocal>().ToListAsync();
+        }
+
+        public async Task<List<TrainingExerciseLocal>> GetItemsNotDoneAsync()
+        {
+            await Init();
+            return await Database.Table<TrainingExerciseLocal>().Where(t => t.Done).ToListAsync();
+
+            // SQL queries are also possible
+            //return await Database.QueryAsync<TodoItem>("SELECT * FROM [TodoItem] WHERE [Done] = 0");
+        }
+
+        public async Task<TrainingExerciseLocal> GetItemAsync(int id)
+        {
+            await Init();
+            return await Database.Table<TrainingExerciseLocal>().Where(i => i.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<int> SaveItemAsync(TrainingExerciseLocal item)
+        {
+            await Init();
+            if (item.Id != 0)
+                return await Database.UpdateAsync(item);
+            else
+                return await Database.InsertAsync(item);
+        }
+
+        public async Task<int> DeleteItemAsync(TrainingExerciseLocal item)
+        {
+            await Init();
+            return await Database.DeleteAsync(item);
+        }
+    }
+}
