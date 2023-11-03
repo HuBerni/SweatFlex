73d252d91c5e45cc6c96751f10ffa6a1b7648206
Bernhard Huber <bernhard.huber2@liebherr.com>
2023-11-03 14:06:33 +0100

    added logic

--- SweatFlex.Maui/ViewModels/CreateExerciseViewModel.cs
+++ SweatFlex.Maui/ViewModels/CreateExerciseViewModel.cs
@@ -1,10 +1,16 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using AutoMapper;
+using CommunityToolkit.Maui.Core.Extensions;
+using CommunityToolkit.Mvvm.ComponentModel;
+using SweatFlex.Maui.Models;
 using SweatFlexAPIClient.Services;
+using SweatFlexData.DTOs.Create;
+using System.Collections.ObjectModel;
 
 namespace SweatFlex.Maui.ViewModels
 {
     public partial class CreateExerciseViewModel : ObservableObject
     {
+        private readonly IMapper _mapper;
         private readonly ExerciseService _exerciseService;
 
         [ObservableProperty]
@@ -14,19 +20,63 @@ namespace SweatFlex.Maui.ViewModels
         private string _description;
 
         [ObservableProperty]
-        private string _musclegroup;
+        private int _musclegroup;
 
         [ObservableProperty]
-        private string _equipment;
+        private int _equipment;
 
         [ObservableProperty]
-        private string _type;
+        private int _type;
 
-        public CreateExerciseViewModel(ExerciseService exerciseService)
+        public ObservableCollection<ExerciseType> ExerciseTypes { get; set; } = new();
+        public ObservableCollection<Musclegroup> Musclegroups { get; set; } = new();
+        public ObservableCollection<Equipment> Equipments { get; set; } = new();
+
+        public CreateExerciseViewModel(IMapper mapper, ExerciseService exerciseService)
         {
+            _mapper = mapper;
             _exerciseService = exerciseService;
         }
 
+        public async Task InitializeAsync()
+        {
+            await SetExerciseAssets();
+        }
 
+        private async Task SetExerciseAssets()
+        {
+            var response = await _exerciseService.GetExerciseAssets();
+
+            if (response.IsSuccess)
+            {
+                ExerciseTypes = response.Result.Types.Select(_mapper.Map<ExerciseType>).ToObservableCollection();
+                Musclegroups = response.Result.MuscleGroups.Select(_mapper.Map<Musclegroup>).ToObservableCollection();
+                Equipments = response.Result.Equipments.Select(_mapper.Map<Equipment>).ToObservableCollection();
+            }
+        }
+
+        public async Task AddExercise(string name, string description, int musclegroupId, int typeId, int? equipmentId = null)
+        {
+            var exerciseCreateDto = new ExerciseCreateDTO()
+            {
+                Creator = Preferences.Get("UserId", ""),
+                Description = description,
+                Musclegroup = musclegroupId,
+                Equipment = equipmentId,
+                Type = typeId,
+                Name = name
+            };
+
+            var response = await _exerciseService.CreateExerciseAsync(exerciseCreateDto);
+
+            if (response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert("Success", "Exercise created successfully", "Ok");
+            }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Something went wrong", "Ok");
+            }
+        }
     }
 }
