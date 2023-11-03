1509b4898c58d401c303e16ed7c828837bc4b684
Bernhard Huber <bernhard.huber2@liebherr.com>
2023-11-03 14:48:48 +0100

    added convertion for selected picker items

--- SweatFlex.Maui/Views/CreateExercise.xaml.cs
+++ SweatFlex.Maui/Views/CreateExercise.xaml.cs
@@ -22,8 +22,15 @@ public partial class CreateExercise : ContentPage
     private async void Save_Clicked(object sender, EventArgs e)
     {
         var musclegroup = MusclegroupPicker.SelectedItem as Musclegroup;
-        var equipment = EquipmentPicker.SelectedItem as Equipment;
         var type = TypePicker.SelectedItem as ExerciseType;
+
+        if (musclegroup == null || type == null)
+        {
+            //TODO add error message
+            return;
+        }
+
+        var equipment = EquipmentPicker.SelectedItem as Equipment;
         await _viewModel.AddExercise(ExerciseName.Text, Description.Text, musclegroup.Id, type.Id, equipment?.Id);
     }
 }
